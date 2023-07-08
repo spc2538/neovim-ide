ec4d5e7adeef3b0d975cd87d953cb61ba2fe0e6d
spc2538 <saul.palestina.cruz@gmail.com>
2023-07-08 10:07:50 -0600

    Issue: Installation script creation

---
+++ install.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+log_file="logfile.log"
+apt_updated=false
+dependencies_packages=("curl" "wget" "git")
+core_packages=("xclip" "fzf" "ripgrep")
+
+create_log_file() {
+  local message="Installing neovim Debian and Ubuntu"
+  echo "$message"
+  echo "$message" > "$log_file"
+}
+
+log_message() {
+  local message="$1"
+  echo "$message"
+  echo "$message" >> "$log_file"
+}
+
+check_and_install_package() {
+  local package="$1"
+  if ! dpkg -s "$package" >/dev/null 2>&1; then
+    log_message "Package '$package' is not installed. Installing..."
+    if ! $apt_updated; then
+      sudo apt-get update
+      apt_updated=true
+    fi
+    sudo apt-get install -y "$package"
+    log_message "Package '$package' has been installed."
+  else
+    log_message "Package '$package' is already installed."
+  fi
+}
+
+command_exists() {
+  local command="$1"
+  command -v "$command" >/dev/null 2>&1
+}
+
+check_and_install_node() {
+  local node_command="node"
+  local npm_command="npm"
+  if command_exists "$node_command" && command_exists "$npm_command"; then
+    log_message "Node.js and npm are already available."
+    local node_version
+    node_version=$(node --version)
+    if [[ $node_version =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
+      local major="${BASH_REMATCH[1]}"
+      local minor="${BASH_REMATCH[2]}"
+      local patch="${BASH_REMATCH[3]}"
+      if (( major < 18 || (major == 18 && minor < 0) )); then
+        local error_message="Node.js version must be 18.0.0 or above. Please reinstall Node.js."
+        log_message "$error_message"
+        echo "$error_message"
+        exit 1
+      fi
+    fi
+    return
+  fi
+
+  log_message "Node.js or npm is not available. Installing..."
+  log_message "Setting up Node.js repository..."
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/nodesource.gpg >/dev/null
+  echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x buster main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  log_message "Updating package lists..."
+  sudo apt update
+  log_message "Installing Node.js..."
+  sudo apt install nodejs -y
+  log_message "Updating package lists again..."
+  sudo apt-get update
+  log_message "Configuring npm..."
+  npm config set prefix '~/.local/'
+  mkdir -p ~/.local/bin
+  echo 'export PATH=~/.local/bin/:$PATH' >> ~/.bashrc
+  source ~/.bashrc
+  log_message "Node.js and npm have been installed."
+
+}
+
+check_and_install_neovim() {
+  # function that checks out if neovim command exists
+}
+
+
+create_log_file
+check_and_install_node
+check_and_install_neovim
+log_message "Installing list of packages"
+
+for package in "${dependencies_packages[@]}"; do
+  check_and_install_package "$package"
+done
+for package in "${core_packages[@]}"; do
+  check_and_install_package "$package"
+done
